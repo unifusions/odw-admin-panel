beb14ceb90b2b8b2f4cb75e06709a12a720a65de
unifusions <siyam@unifusions.com>
2025-04-05 13:44:31 +0530

    v0.1.31

--- app/Models/Admin/Clinic.php
+++ app/Models/Admin/Clinic.php
@@ -3,12 +3,13 @@
 namespace App\Models\Admin;
 
 use App\ClinicScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Clinic extends Model
 {
     
-    use ClinicScope;
+    use ClinicScope, HasFactory;
     protected $fillable = [
         'name',
        
