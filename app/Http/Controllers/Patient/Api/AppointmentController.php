028673c9833d9938fa0d410b7c94f0cc206ebffb
unifusions <siyam@unifusions.com>
2025-04-14 13:03:09 +0530

    v0.2.8 Added Api Appointment

---
+++ app/Http/Controllers/Patient/Api/AppointmentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Patient\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function bookAppointment(Request $request)
+    {
+
+        // Check if slot is already booked
+        $isBooked = Appointment::where('appointment_date', $request->appointment_date)
+            ->where('clinic_dentist_id', $request->doctor_id)
+            ->where('time_slot', $request->time_slot)
+            ->exists();
+
+        if ($isBooked) {
+            return response()->json(['error' => 'This time slot is no longer available.'], 422);
+        }
+
+        // Create Appointment
+        $appointment = Appointment::create([
+            'clinic_id' => $request->clinic_id,
+            'clinic_branch_id' => $request->clinic_branch_id,
+            'clinic_dentist_id' => $request->doctor_id ?? null,
+            'patient_id' => $request->patient_id,
+            'appointment_date' => $request->appointment_date,
+            'time_slot' => $request->timeSlot,
+            'status' => 'pending',
+        ]);
+
+        return response()->json(['success' => 'This time slot is no longer available.'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Patient\Api\AppointmentController;
 use App\Http\Controllers\Patient\ClinicController;
 use App\Http\Controllers\Patient\DealsController;
 use App\Http\Controllers\Patient\DentalServiceController;
@@ -14,15 +15,16 @@ Route::post('/register', [RegistrationController::class, 'register']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
- Route::get('/deals', DealsController::class);
+
 // Route::get('/clinics', ClinicController::class);
 
-Route::get('/clinics', ClinicController::class);
 
 //ADD URLS BELOW SANCTUM 
 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/dental-services', DentalServiceController::class);
-   
+    Route::get('/deals', DealsController::class);
+    Route::get('/clinics', ClinicController::class);
+    Route::post('/book-appointment', [AppointmentController::class, 'bookAppointment']);
 });
