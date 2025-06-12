87e8e9d1cba27f43afd00c77df8ecbf8601d26ad
unifusions <siyam@unifusions.com>
2025-06-12 22:22:40 +0530

    v0.3.66

--- app/Http/Controllers/Patient/Api/AppointmentController.php
+++ app/Http/Controllers/Patient/Api/AppointmentController.php
@@ -12,8 +12,8 @@ class AppointmentController extends Controller
 
     public function myAppointment(Request $request){
        $patient = $request->patient_id;
-       $openBookings = Appointment::where('status', 'confirmed')->where('patient_id', $patient)->whereDate('appointment_date', '>=', now())->get();
-       $pendingBookings = Appointment::where('status','pending')->where('patient_id', $patient)->whereDate('appointment_date', '>=', now())->orderBy('appointment_date', 'asc')->get();
+       $openBookings = Appointment::where('status', 'confirmed')->where('patient_id', $patient)->whereDate('appointment_date', '> ', now())->orderBy('appointment_date', 'asc')->get();
+       $pendingBookings = Appointment::where('status','pending')->where('patient_id', $patient)->whereDate('appointment_date', '>', now())->orderBy('appointment_date', 'asc')->get();
        return response()->json([
         'open'=>$openBookings,
         'pending' =>  $pendingBookings->map(function ($booking){
