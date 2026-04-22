4c55fb55a77429f2cbb3682994a3ce84e24b9000
unifusions <siyam@unifusions.com>
2026-04-22 21:20:36 +0530

    tessting city

--- app/Http/Controllers/Patient/RegistrationController.php
+++ app/Http/Controllers/Patient/RegistrationController.php
@@ -22,6 +22,7 @@ class RegistrationController extends Controller
     public function login(Request $request)
     {
 
+    dd($request->cityName);
         $input = $request->input('loginInput');
         $isEmail = false;
         $isPhone = false;
