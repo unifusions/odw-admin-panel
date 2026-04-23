223d90d43a290aa119831d460748ea0381ee5b84
unifusions <siyam@unifusions.com>
2026-04-23 16:17:01 +0530

    composer 8.3

--- app/Http/Controllers/Patient/RegistrationController.php
+++ app/Http/Controllers/Patient/RegistrationController.php
@@ -22,7 +22,7 @@ class RegistrationController extends Controller
     public function login(Request $request)
     {
 
-    dd($request->cityName);
+    return $request->cityName ;
         $input = $request->input('loginInput');
         $isEmail = false;
         $isPhone = false;
